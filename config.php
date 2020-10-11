d00ba929d76d959d2c15ddbb72aaaf26edfb010b
César Gómez <68523595+cesargomez93@users.noreply.github.com>
2020-10-11 16:16:29 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,5 @@
 <?php
 
-$db = ['host' => 'localhost', 'username' => 'root', 'password' => 'cesar123', 'db' => 'restaurante'];
-//$db = ['host' => 'ec2-18-208-126-21.compute-1.amazonaws.com', 'username' => 'myuser', 'password' => 'mypass', 'db' => 'restaurante'];
-//$db = ['host' => '45.93.101.52', 'username' => 'u743804485_myuser', 'password' => 'M4qu1n4ws', 'db' => 'u743804485_restaurante'];
+$db = ['host' => 'mysql1', 'username' => 'myuser', 'password' => 'mypass', 'db' => 'restaurante'];
 
 ?>
